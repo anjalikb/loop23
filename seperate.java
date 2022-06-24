7174138e945fee79c9b60f63e192a735a0985b48
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-06-24 16:43:00 +0530

    Add files via upload

---
+++ seperate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class seperate
+{
+  public static void main(String args[])
+{
+   
+   int a= 12345 , i,rem;
+   i=a;
+   while(i!=0)
+  {
+   rem=i%10;
+   System.out.println(rem +"");
+   i=i/10;
+  }
+}
+}
+   
